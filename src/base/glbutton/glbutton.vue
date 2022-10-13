62a77ddcb473447c619d79bd467fbd5e5bdbda8b
zhangbingshuai <1533644421@qq.com>
2022-10-13 11:17:28 +0800

    update

---
+++ src/base/glbutton/glbutton.vue
@@ -0,0 +1,23 @@
+<template>
+    <button @click="handleClick" :disabled='disabled'>
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+
+export default {
+    name: "GlButton",
+    props: {
+        disabled: Boolean
+    },
+    components: {
+        HelloWorld,
+    },
+    methods: {
+        handleClick(evt) {
+            this.$emit('click', evt);
+        }
+    }
+};
+</script>
---
+++ src/base/index.js
@@ -0,0 +1,9 @@
+
+  import GlButton from './src/button';
+
+/* istanbul ignore next */
+GlButton.install = function(Vue) {
+  Vue.component(GlButton.name, GlButton);
+};
+
+export default GlButton;
